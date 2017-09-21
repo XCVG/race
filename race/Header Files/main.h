767d050e914aa652ad13d57633da4d1642bdeab1
Spencer Pollock <srepollock@gmail.com>
2017-09-21 10:21:52 -0700

    Updated header file for main.h
    The update will pull different header files per operating system. This will allow us to build with Mac or Windows

--- race/Header Files/main.h
+++ race/Header Files/main.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#ifdef __APPLE__
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_opengl.h>
+#include <GLUT/glut.h>
+#include <GLFW/glfw3.h>
+#elif defined _WIN32 || defined _WIN64
+#include <SDL.h>
+#include <GL/glut.h>
+#include <GL/GL.h>
+#endif 
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
