1d4fcde6cd327a8fe52ceb3e34c45facd20175db
Spencer Pollock <srepollock@gmail.com>
2017-09-25 13:36:51 -0700

    Fixed headers for mac
    Will have to create this branch a lot for hotfixes

--- race/GameEngine/HeaderFiles/OBJImport.h
+++ race/GameEngine/HeaderFiles/OBJImport.h
@@ -1,14 +1,19 @@
 #pragma once
-#ifndef OBJIMPORT_H
-#define OBJIMPORT_H
+
+#ifdef __APPLE__
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_filesystem.h>
+#elif defined _WIN32 || defined _WIN64
+#include <glew.h>
+#include <SDL.h>
+#include <SDL_filesystem.h>
+#endif 
 #include <string>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <stdlib.h>
-#include <glew.h>
-#include <SDL.h>
-#include <SDL_filesystem.h>
 
 class OBJImport {
 public:
@@ -20,5 +25,3 @@ public:
 	static std::vector<GLfloat> importObjInfo(std::string string);
 
 };
-
-#endif
