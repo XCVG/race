812c528529a495d84eee45cbe312f46cc7917052
Spencer Pollock <spencer@spollock.ca>
2017-09-27 22:39:40 -0700

    :tired_face: Added multi-threading for the game. Have things working that the engine will have control of the physics and ai thread once created. Also going to pass off the engine thread to the main UI thread so that things can be shutdown properly. Or there will be a shutdown signal from the message system through the main thread that will signal the engine to stop and shutdown

--- race/GameEngine/SourceFiles/Engine.cpp
+++ race/GameEngine/SourceFiles/Engine.cpp
@@ -3,43 +3,72 @@
 
 void Engine::start() {
     // Create the other engines, or at least get pointer to them
-    _renderEngine = new RenderEngine();
-    if (_renderEngine == nullptr) {
+    _renderEngine_p = new RenderEngine();
+    if (_renderEngine_p == nullptr) {
         std::cout << ErrorHandler::getErrorString(1) << std::endl;
     }
-    _physicsEngine = new PhysicsEngine();
-    if (_physicsEngine == nullptr) {
+    _physicsEngine_p = new PhysicsEngine();
+    if (_physicsEngine_p == nullptr) {
         std::cout << ErrorHandler::getErrorString(1) << std::endl;
     }
-    _aiEngine = new AIEngine();
-    if (_aiEngine == nullptr) {
+    _aiEngine_p = new AIEngine();
+    if (_aiEngine_p == nullptr) {
         std::cout << ErrorHandler::getErrorString(1) << std::endl;
     }
-    _inputEngine = new InputEngine();
-    if (_inputEngine == nullptr) {
+    _inputEngine_p = new InputEngine();
+    if (_inputEngine_p == nullptr) {
         std::cout << ErrorHandler::getErrorString(1) << std::endl;
     }
-    _soundEngine = new SoundEngine();
-    if (_soundEngine == nullptr) {
+    _soundEngine_p = new SoundEngine();
+    if (_soundEngine_p == nullptr) {
         std::cout << ErrorHandler::getErrorString(1) << std::endl;
     }
     try {
-        _renderEngine->start();
-        _physicsEngine->start();
-        _aiEngine->start();
-        _inputEngine->start();
-        _soundEngine->start();
+        _renderEngine_p->start();
+        _physicsThread_p = _physicsEngine_p->start();
+        _aiThread_p = _aiEngine_p->start();
+        _inputEngine_p->start();
+        _soundEngine_p->start();
     } catch (std::exception e) {
         std::cout << ErrorHandler::getErrorString(1) << std::endl;
+    }
+    _engineThread_p = new std::thread(&Engine::loop, this);
+    if (!_running) {
+        _running = true;
+    } else {
+        this->stop();
+        std::cout << ErrorHandler::getErrorString(1) << std::endl;
+        delete this;
     }
 }
 
 void Engine::update() {
-
+    SDL_Log("%s", "Running Engine::udpate");
 }
 
 void Engine::loop() {
+    if (!_running) {
+        return;
+    }
+    while(_running) {
+        this->update();
+    }
+}
 
+///
+/// <title>
+/// Engine Stop
+/// </title>
+/// <summary>
+/// Will stop the engine from running. This will halt the main loop and other 
+/// threads. The main purpose is to halt the game here. This can be a 
+/// catch-all for other cleanup that is required in a stop/start of the game.
+/// </summary>
+/// 
+void Engine::stop() {
+    _running = false;
+    _physicsEngine_p->stop();
+    _aiEngine_p->stop();
 }
 
 Engine::Engine() {
@@ -47,9 +76,9 @@ Engine::Engine() {
 }
 
 Engine::~Engine() {
-    _soundEngine->~SoundEngine();
-    _inputEngine->~InputEngine();
-    _aiEngine->~AIEngine();
-    _physicsEngine->~PhysicsEngine();
-    _renderEngine->~RenderEngine();
+    _soundEngine_p->~SoundEngine();
+    _inputEngine_p->~InputEngine();
+    _aiEngine_p->~AIEngine();
+    _physicsEngine_p->~PhysicsEngine();
+    _renderEngine_p->~RenderEngine();
 }
