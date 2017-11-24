3f0179d2fed9dcbd3996f01c7c5633dea1049531
Spencer Pollock <spencer@spollock.ca>
2017-11-24 13:00:46 -0800

    :+1: Added get vertices only from obj file

--- race/GameEngine/Components/SourceFiles/BoxColliderComponent.cpp
+++ race/GameEngine/Components/SourceFiles/BoxColliderComponent.cpp
@@ -2,7 +2,11 @@
 BoxColliderComponent::BoxColliderComponent(std::string filename)
 {
     std::string fileContents = FileHelper::loadFileFromStringRelative(MODEL_BASEPATH_CONST + filename + MODEL_EXTENSION_CONST);
-    std::vector<GLfloat> vertices = OBJImport::importObjInfo(fileContents);
+    std::vector<GLfloat> vertices = OBJImport::importObjInfoVertices(fileContents);
+    for (int i = 0; i < vertices.size() - 1; i+=3)
+    {
+        if (this->_minX > vertices.at(i)) this->_minX = vertices.at(i);
+    }
 }
 
 void BoxColliderComponent::setMinX(int _x)
