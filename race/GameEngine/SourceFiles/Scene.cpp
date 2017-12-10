f1e6cc085b34cdcf51a71a7b0b29400c657519a1
Spencer Pollock <srepollock@gmail.com>
2017-12-09 17:48:59 -0800

    Updates speed and acceleration

--- race/GameEngine/SourceFiles/Scene.cpp
+++ race/GameEngine/SourceFiles/Scene.cpp
@@ -145,11 +145,11 @@ void Scene::setUpSceneOne() {
 	go->addComponent(new RenderComponent("raceTrack", "test_texture2", "", 0));
 	addGameObject("Road", go);*/
 	
-	go = new GameObject(new Transform(new Vector3(0, 0, 0), new Vector3(0, 0, 0), 1.0f));
+	go = new GameObject(new Transform(new Vector3(0, 0, 0), new Vector3(0, 0, 0), 3.0f));
 	go->addComponent(new RenderComponent("track1a", "grass", "", 0));
 	addGameObject("Ground", go);
 
-	go = new GameObject(new Transform(new Vector3(0, -0.9f, 0), new Vector3(0, 0, 0), 1.0f));
+	go = new GameObject(new Transform(new Vector3(0, -2.9f, 0), new Vector3(0, 0, 0), 3.0f));
 	go->addComponent(new RenderComponent("track1b", "asphault", "", 0));
 	addGameObject("Road", go);
 
